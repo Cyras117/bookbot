741a551b2e8b2edd28e3f41fbf7ca0a7dd933e08
Cyras117 <alexsandro.oliveira117@gmail.com>
2025-07-06 18:54:44 -0400

    feat:reading book file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+
+def get_book_text(self, filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    filepath = "/home/saryc/bootdev/projects/bookbot/books/frankenstein.txt"
+    text = get_book_text(None, filepath)
+    print(text)
+
+if __name__ == "__main__":
+    main()
