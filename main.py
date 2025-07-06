d9ca16be421dd2276582d8ee06a7da904fb88575
Cyras117 <alexsandro.oliveira117@gmail.com>
2025-07-06 19:02:54 -0400

    feat:count number of words

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ def get_book_text(self, filepath):
     with open(filepath) as f:
         return f.read()
 
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
 def main():
     filepath = "/home/saryc/bootdev/projects/bookbot/books/frankenstein.txt"
     text = get_book_text(None, filepath)
-    print(text)
+    num_words = get_word_count(text)
+    print(f"{num_words} words found in the document")
 
 if __name__ == "__main__":
     main()
